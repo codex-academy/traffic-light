17f82a81a92b540409176a0c403de3f0f070d26f
avermeulen <andre.vermeulen@gmail.com>
2016-09-30 15:11:18 +0200

    fixed overridden file

--- conditional-logic.js
+++ conditional-logic.js
@@ -3,5 +3,5 @@ captureTimeChange(captureTrafficLightChange);
 function captureTrafficLightChange(number){
     //write your code here!
     // number is the value entered in the text on the screen
-
+    
 }
--- traffic-light.js
+++ traffic-light.js
@@ -1,5 +1,4 @@
 
-
 function allLightsOff(){
   document.querySelectorAll('.light').forEach(function(light){
     light.classList.remove('on');
@@ -25,9 +24,11 @@ function caution(){
 }
 
 function captureTimeChange(cb){
-
   var timeLeft = document.querySelector("#timeLeft");
   var number = document.querySelector("#number");
+
+  console.log(timeLeft);
+
   timeLeft.addEventListener("keyup", function(evt){
       var num = Number(evt.target.value);
       number.innerHTML = num;
